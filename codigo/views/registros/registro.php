105f44f9b0d270f786fbba8e94568b19f0310be7
marcos-diez <marcos_diez@usal.es>
2025-01-01 17:24:49 +0100

    Add files via upload

--- codigo/views/registros/registro.php
+++ codigo/views/registros/registro.php
@@ -1,5 +1,7 @@
-<?php use yii\widgets\ActiveForm;
+<?php use yii\widgets\ActiveForm; 
 use yii\helpers\Html;
+use yii\helpers\Json;
+use yii\web\JsExpression;
 
 /* @var $this yii\web\View */
 /* @var $model app\models\Usuario */
@@ -16,29 +18,42 @@ $this->title = 'Registro de Usuario';
     ]); ?>
 
     <?= $form->field($model, 'email')->textInput(['maxlength' => true]) ?>
-
     <?= $form->field($model, 'password')->passwordInput(['maxlength' => true]) ?>
-
     <?= $form->field($model, 'password_repeat')->passwordInput(['maxlength' => true]) ?>
-
     <?= $form->field($model, 'nick')->textInput(['maxlength' => true]) ?>
-
     <?= $form->field($model, 'nombre')->textInput(['maxlength' => true]) ?>
-
     <?= $form->field($model, 'apellidos')->textInput(['maxlength' => true]) ?>
-
     <?= $form->field($model, 'telefono')->textInput(['maxlength' => true]) ?>
-
     <?= $form->field($model, 'direccion')->textarea(['rows' => 4]) ?>
-
     <?= $form->field($model, 'fecha_nacimiento')->input('date') ?>
 
     <!-- Opción para la región -->
-    <?= $form->field($model, 'region_id')->dropDownList([
-        1 => 'Región 1', 
-        2 => 'Región 2', 
-        3 => 'Región 3',
-    ], ['prompt' => 'Selecciona tu región']) ?>
+    <?= $form->field($model, 'region_id')->dropDownList(
+		\yii\helpers\ArrayHelper::map($regionesPadre, 'id', 'nombre'),
+		[
+			'prompt' => 'Selecciona tu continente',
+			'id' => 'region-continent'
+		]
+	)
+	 ?>
+
+    <?= $form->field($model, 'region_id')->dropDownList(
+        [],
+        [
+            'prompt' => 'Selecciona tu país',
+            'id' => 'region-country',
+            'disabled' => true
+        ]
+    ) ?>
+
+    <?= $form->field($model, 'region_id')->dropDownList(
+        [],
+        [
+            'prompt' => 'Selecciona tu provincia',
+            'id' => 'region-province',
+            'disabled' => true
+        ]
+    ) ?>
 
     <div class="form-group">
         <?= Html::submitButton('Registrarme', ['class' => 'btn btn-primary']) ?>
@@ -46,3 +61,47 @@ $this->title = 'Registro de Usuario';
 
     <?php ActiveForm::end(); ?>
 </div>
+
+<?php
+$script = <<< JS
+    $('#region-continent').change(function() {
+        var continentId = $(this).val();
+        if (continentId) {
+            $.ajax({
+                url: '/path/to/your/controller/action', // Cambiar al endpoint correcto
+                type: 'GET',
+                data: {id: continentId},
+                success: function(data) {
+                    var countries = JSON.parse(data);
+                    $('#region-country').html('');
+                    $.each(countries, function(index, value) {
+                        $('#region-country').append('<option value="'+index+'">'+value+'</option>');
+                    });
+                    $('#region-country').prop('disabled', false);
+                }
+            });
+        }
+    });
+
+    $('#region-country').change(function() {
+        var countryId = $(this).val();
+        if (countryId) {
+            $.ajax({
+                url: '/path/to/your/controller/action', // Cambiar al endpoint correcto
+                type: 'GET',
+                data: {id: countryId},
+                success: function(data) {
+                    var provinces = JSON.parse(data);
+                    $('#region-province').html('');
+                    $.each(provinces, function(index, value) {
+                        $('#region-province').append('<option value="'+index+'">'+value+'</option>');
+                    });
+                    $('#region-province').prop('disabled', false);
+                }
+            });
+        }
+    });
+JS;
+
+$this->registerJs($script);
+?>
