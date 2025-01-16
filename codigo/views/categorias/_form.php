80641f47a926cab2d740e5b05946627656381553
marcosmendez23 <marcos_men_rod@usal.es>
2025-01-16 19:55:20 +0100

    Update _form.php

--- codigo/views/categorias/_form.php
+++ codigo/views/categorias/_form.php
@@ -16,10 +16,6 @@ use yii\widgets\ActiveForm;
 
     <?= $form->field($model, 'descripcion')->textarea(['rows' => 6]) ?>
 
-    <?= $form->field($model, 'icono')->textInput(['maxlength' => true]) ?>
-
-    <?= $form->field($model, 'categoria_padre_id')->textInput() ?>
-
     <div class="form-group">
         <?= Html::submitButton(Yii::t('app', 'Save'), ['class' => 'btn btn-success']) ?>
     </div>
