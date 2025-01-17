1a89798a7a902ebef1111dd7b518bbd8e2b7fc22
victormarcam16 <victormarcam16@gmail.com>
2025-01-17 20:03:07 +0100

    Add files via upload

---
+++ codigo/views/site/contact.php
@@ -0,0 +1,16 @@
+<?php
+use yii\helpers\Html;
+
+$this->title = 'Contacto';
+?>
+
+<div class="site-contact">
+    <h1><?= Html::encode($this->title) ?></h1>
+    <p>Si necesitas ponerte en contacto con nosotros, aquí tienes nuestros datos de contacto:</p>
+    <ul>
+        <li><strong>Teléfono:</strong> +34 123 456 789</li>
+        <li><strong>Email:</strong> contacto@comparadorprecios.com</li>
+        <li><strong>Dirección:</strong> Campus Viriato, Zamora, España</li>
+    </ul>
+    <p>Estamos disponibles de lunes a viernes de 9:00 a 18:00. ¡No dudes en comunicarte con nosotros para cualquier consulta o sugerencia!</p>
+</div>
