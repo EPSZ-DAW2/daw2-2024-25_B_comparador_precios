cb17fe9e12b1b8d68523d6344132430fd307a556
javimarinas <jmarinasfdez@usal.es>
2025-01-17 20:05:04 +0100

    Add files via upload

--- codigo/controllers/TiendasController.php
+++ codigo/controllers/TiendasController.php
@@ -723,14 +723,6 @@ class TiendasController extends Controller
 			// Capturar el motivo desde el formulario
 			$nuevoMotivo = Yii::$app->request->post('motivo_denuncia');
 
-			// Incrementar el contador de denuncias
-			$tienda->denuncias = $tienda->denuncias + 1;
-
-			// Registrar la fecha de la primera denuncia (si no se ha registrado antes)
-			if (!$tienda->fecha_primera_denuncia) {
-				$tienda->fecha_primera_denuncia = date('Y-m-d H:i:s');
-			}
-
 			// Agregar el nuevo motivo de denuncia a la tienda
 			$tienda->agregarMotivoDenuncia($nuevoMotivo);
 
@@ -770,8 +762,6 @@ class TiendasController extends Controller
 			$seguimiento = new Seguimiento();
 			$seguimiento->usuario_id = $usuarioId;
 			$seguimiento->tienda_id = $id;
-			$seguimiento->articulo_id = null; // ***¡Importante que sea NULL!***
-			$seguimiento->oferta_id = null; // ***¡Importante que sea NULL!***
 			$seguimiento->fecha = date('Y-m-d H:i:s');
 			if ($seguimiento->save()) {
 				Yii::$app->session->setFlash('success', 'Ahora sigues esta tienda.');
