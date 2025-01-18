bc0e074ef7ed3e3961ac65ee2878c017ccf79d09
javimarinas <jmarinasfdez@usal.es>
2025-01-18 01:35:58 +0100

    Update login.php

--- codigo/views/site/login.php
+++ codigo/views/site/login.php
@@ -15,6 +15,19 @@ $this->params['breadcrumbs'][] = $this->title;
 
     <p>Please fill out the following fields to login:</p>
 
+    <!-- Mostrar mensajes flash -->
+    <?php if (Yii::$app->session->hasFlash('success')): ?>
+        <div class="alert alert-success">
+            <?= Yii::$app->session->getFlash('success') ?>
+        </div>
+    <?php endif; ?>
+
+    <?php if (Yii::$app->session->hasFlash('error')): ?>
+        <div class="alert alert-danger">
+            <?= Yii::$app->session->getFlash('error') ?>
+        </div>
+    <?php endif; ?>
+    
     <?php $form = ActiveForm::begin([
         'id' => 'login-form',
         'layout' => 'horizontal',
