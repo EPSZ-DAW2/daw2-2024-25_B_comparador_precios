c53cd0db7c8373f88739858ed1619080615f9136
victormarcam16 <victormarcam16@gmail.com>
2025-01-19 17:21:27 +0100

    Add files via upload

---
+++ codigo/views/ofertas/index.php
@@ -0,0 +1,28 @@
+<?php
+
+use yii\widgets\ListView;
+use yii\helpers\Html;
+
+/** @var yii\web\View $this */
+/** @var app\models\OfertasSearch $searchModel */
+/** @var yii\data\ActiveDataProvider $dataProvider */
+
+$this->title = Yii::t('app', 'Ofertas');
+$this->params['breadcrumbs'][] = $this->title;
+?>
+<div class="tienda-index"> <!-- Cambiar de 'ofertas-index' a 'tienda-index' -->
+
+    <h1><?= Html::encode($this->title) ?></h1>
+
+    <!-- Renderiza el formulario de búsqueda -->
+    <?= $this->render('_search', ['model' => $searchModel]) ?>
+
+    <!-- ListView para mostrar los resultados como fichas -->
+    <?= ListView::widget([
+        'dataProvider' => $dataProvider,
+        'itemView' => '_oferta_card', // Vista parcial para cada ficha
+        'layout' => "<div class='tienda-list'>{items}</div>\n{pager}", // Estructura de la lista
+        'emptyText' => '<p>No se encontraron ofertas.</p>',
+    ]) ?>
+
+</div>
