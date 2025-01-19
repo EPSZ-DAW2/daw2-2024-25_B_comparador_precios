2732f9ab95ce3ff0d0bf03fc02b570f17a1a5250
victormarcam16 <victormarcam16@gmail.com>
2025-01-19 17:20:32 +0100

    Add files via upload

---
+++ codigo/views/public-articulos/public-index.php
@@ -0,0 +1,30 @@
+<?php
+
+use yii\widgets\ListView;
+use yii\helpers\Html;
+
+/** @var yii\web\View $this */
+/** @var app\models\ArticulosSearch $searchModel */
+/** @var yii\data\ActiveDataProvider $dataProvider */
+
+$this->title = Yii::t('app', 'Artículos');
+$this->params['breadcrumbs'][] = $this->title;
+?>
+<div class="tienda-index"> <!-- Cambiar de 'articulo-index' a 'tienda-index' -->
+
+    <h1><?= Html::encode($this->title) ?></h1>
+
+    <!-- Renderiza el formulario de búsqueda -->
+    <?= $this->render('_search', ['model' => $searchModel]) ?>
+
+    <!-- ListView para mostrar los resultados como fichas -->
+    <?= ListView::widget([
+        'dataProvider' => $dataProvider,
+        'itemView' => '_articulo_card', // Vista parcial para cada ficha
+        'layout' => "<div class='tienda-list'>{items}</div>\n{pager}", // Estructura de la lista
+        'emptyText' => '<p>No se encontraron artículos.</p>',
+    ]) ?>
+
+</div>
+
+
