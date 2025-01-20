0092280b5f811de651d347f07f14d479bb2de01a
victormarcam16 <victormarcam16@gmail.com>
2025-01-21 00:42:10 +0100

    Add files via upload

--- codigo/views/tiendas-admin/index.php
+++ codigo/views/tiendas-admin/index.php
@@ -26,38 +26,77 @@ $this->params['breadcrumbs'][] = $this->title;
         'filterModel' => $searchModel,
         'columns' => [
             ['class' => 'yii\grid\SerialColumn'],
-            'id',
-            'nombre',
-            'descripcion:ntext',
-            'lugar',
-            'url',
-            'direccion:ntext',
-            'region_id',
-            'clasificacion_id',
-            'visible:boolean',
-            'cerrada:boolean',
-            'suma_valoraciones',
-            'suma_votos',
-			[
+            [
+                'attribute' => 'id',
+                'filter' => Html::activeTextInput($searchModel, 'id', ['class' => 'form-control']), 
+            ],
+            [
+                'attribute' => 'nombre',
+                'filter' => Html::activeTextInput($searchModel, 'nombre', ['class' => 'form-control']),
+            ],
+            [
+                'attribute' => 'descripcion',
+                'format' => 'ntext',
+                'filter' => Html::activeTextInput($searchModel, 'descripcion', ['class' => 'form-control']),
+            ],
+            [
+                'attribute' => 'lugar',
+                'filter' => Html::activeTextInput($searchModel, 'lugar', ['class' => 'form-control']),
+            ],
+            [
+                'attribute' => 'url',
+                'filter' => Html::activeTextInput($searchModel, 'url', ['class' => 'form-control']),
+            ],
+            [
+                'attribute' => 'direccion',
+                'format' => 'ntext',
+                'filter' => Html::activeTextInput($searchModel, 'direccion', ['class' => 'form-control']),
+            ],
+            [
+                'attribute' => 'region_id',
+                'filter' => Html::activeTextInput($searchModel, 'region_id', ['class' => 'form-control']),
+            ],
+            [
+                'attribute' => 'clasificacion_id',
+                'filter' => Html::activeTextInput($searchModel, 'clasificacion_id', ['class' => 'form-control']),
+            ],
+            [
+                'attribute' => 'visible',
+                'filter' => Html::activeTextInput($searchModel, 'visible', ['class' => 'form-control']),
+            ],
+            [
+                'attribute' => 'cerrada',
+                'filter' => Html::activeTextInput($searchModel, 'cerrada', ['class' => 'form-control']),
+            ],
+            [
+                'attribute' => 'suma_valoraciones',
+                'filter' => Html::activeTextInput($searchModel, 'suma_valoraciones', ['class' => 'form-control']),
+            ],
+            [
+                'attribute' => 'suma_votos',
+                'filter' => Html::activeTextInput($searchModel, 'suma_votos', ['class' => 'form-control']),
+            ],
+            [
                 'attribute' => 'denuncias',
                 'label' => 'Denuncias',
                 'value' => function ($model) {
                     return $model->denuncias ?: 0;
                 },
+                'filter' => Html::activeTextInput($searchModel, 'denuncias', ['class' => 'form-control']),
             ],
             [
-				'class' => 'yii\grid\ActionColumn',
-				'template' => '{view} {update} {delete} {denuncias}',
-				'buttons' => [
-					'denuncias' => function ($url, $model) {
-						return Html::a(
-							'<i class="fa fa-warning"></i> Denuncias',
-							['tiendas-admin/gestion-denuncias', 'id' => $model->id],
-							['class' => 'btn btn-warning btn-sm']
-						);
-					},
-				],
-			],
+                'class' => ActionColumn::class,
+                'template' => '{view} {update} {delete} {denuncias}',
+                'buttons' => [
+                    'denuncias' => function ($url, $model) {
+                        return Html::a(
+                            '<i class="fa fa-warning"></i> Denuncias',
+                            ['tiendas-admin/gestion-denuncias', 'id' => $model->id],
+                            ['class' => 'btn btn-warning btn-sm', 'title' => 'Gestionar denuncias']
+                        );
+                    },
+                ],
+            ],
         ],
     ]); ?>
 
