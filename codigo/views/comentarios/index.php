91ba38e04e1a3aa988646ec629b9b8e203f625ae
marcosmendez23 <marcos_men_rod@usal.es>
2025-01-20 14:28:17 +0100

    Add files via upload

--- codigo/views/comentarios/index.php
+++ codigo/views/comentarios/index.php
@@ -2,9 +2,9 @@
 
 use yii\grid\GridView;
 use yii\helpers\Html;
-use yii\helpers\Url;
 
 /** @var yii\web\View $this */
+/** @var app\models\ComentariosSearch $searchModel */
 /** @var yii\data\ActiveDataProvider $dataProvider */
 
 $this->title = 'Comentarios';
@@ -14,19 +14,38 @@ $this->params['breadcrumbs'][] = $this->title;
 
     <h1><?= Html::encode($this->title) ?></h1>
 
+    <?= Html::a(Yii::t('app', 'Create Comentario'), ['create'], ['class' => 'btn btn-success']) ?>
+
     <?= GridView::widget([
         'dataProvider' => $dataProvider,
+        'filterModel' => $searchModel,
         'columns' => [
             ['class' => 'yii\grid\SerialColumn'],
 
-            'id',
+            // Columna para ID con placeholder personalizado
+            [
+                'attribute' => 'id',
+                'label' => 'ID',
+                'value' => 'id',
+                'filter' => Html::activeTextInput($searchModel, 'id', [
+                    'class' => 'form-control',
+                    'placeholder' => 'Buscar por ID...', // Placeholder aquí
+                ]),
+            ],
+
+            // Columna para Contenido con placeholder
             [
                 'attribute' => 'texto',
                 'label' => 'Contenido',
                 'value' => function ($model) {
                     return substr($model->texto, 0, 50) . '...';
                 },
+                'filter' => Html::activeTextInput($searchModel, 'contenido', [
+                    'class' => 'form-control',
+                    'placeholder' => 'Buscar contenido...',
+                ]),
             ],
+
             'denuncias',
             [
                 'attribute' => 'bloqueado',
@@ -34,15 +53,18 @@ $this->params['breadcrumbs'][] = $this->title;
                 'value' => function ($model) {
                     return $model->bloqueado ? 'Bloqueado' : 'Activo';
                 },
+                'filter' => [0 => 'Activo', 1 => 'Bloqueado'],
             ],
             [
                 'class' => 'yii\grid\ActionColumn',
                 'template' => '{view} {update} {delete} {gestion-denuncias}',
                 'buttons' => [
                     'gestion-denuncias' => function ($url, $model) {
-                        return Html::a('Denuncias', ['gestion-denuncias', 'id' => $model->id], [
-                            'class' => 'btn btn-warning btn-sm',
-                        ]);
+                        return Html::a(
+                            '<i class="fa fa-warning"></i> Denuncias',
+                            ['comentarios/gestion-denuncias', 'id' => $model->id],
+                            ['class' => 'btn btn-warning btn-sm']
+                        );
                     },
                 ],
             ],
