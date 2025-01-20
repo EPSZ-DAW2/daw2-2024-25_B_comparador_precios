815d9496224641882c949e60d8c3ca2db85a59e1
Alejandra García Martín <148661409+alebigfoot@users.noreply.github.com>
2025-01-20 23:07:34 +0100

    arreglado admin tienda

--- codigo/views/tiendas/admin-tienda.php
+++ codigo/views/tiendas/admin-tienda.php
@@ -2,43 +2,38 @@
 use yii\helpers\Html;
 
 /** @var yii\web\View $this */
-/** @var app\models\Tienda[] $tiendas*/
+/** @var app\models\Tienda[] $tiendas */
 
 $this->title = 'Panel de Administración de Tiendas';
 $this->params['breadcrumbs'][] = $this->title;
 ?>
 <div class="tienda-admin">
     <h1><?= Html::encode($this->title) ?></h1>
-
+    <h2>Número de tiendas disponibles: <?= count($tiendas) ?></h2>
     <?php if (!empty($tiendas)): ?>
-        <div class="panel-group" id="accordion">
+        <div class="panel-group">
             <?php foreach ($tiendas as $tienda): ?>
                 <div class="panel panel-default">
                     <div class="panel-heading">
                         <h4 class="panel-title">
-                            <a data-toggle="collapse" data-parent="#accordion" href="#collapse-<?= $tienda->id ?>">
-                                <?= Html::encode($tienda->razon_social) ?>
-                            </a>
+                            <?= Html::encode($tienda->razon_social) ?>
                         </h4>
                     </div>
-                    <div id="collapse-<?= $tienda->id ?>" class="panel-collapse collapse">
-                        <div class="panel-body">
-                            <p>
+                    <div class="panel-body">
+                        <p>
                             <?= Html::a('Ver Tienda', ['view-store', 'id' => $tienda->id], ['class' => 'btn btn-info']) ?>
-                                <?= Html::a('Crear Oferta', ['crear-oferta', 'Tienda_id' => $tienda->id], ['class' => 'btn btn-success']) ?>
-                                <?= Html::a('Modificar Oferta', ['modificar-oferta', 'Tienda_id' => $tienda->id], ['class' => 'btn btn-primary']) ?>
-                                <?= Html::a('Eliminar Oferta', ['eliminar-oferta', 'Tienda_id' => $tienda->id], ['class' => 'btn btn-danger']) ?>
-                                <?= Html::a('Crear Artículo', ['crear-articulo', 'Tienda_id' => $tienda->id], ['class' => 'btn btn-success']) ?>
-                                <?= Html::a('Eliminar Artículo', ['eliminar-articulo', 'Tienda_id' => $tienda->id], ['class' => 'btn btn-danger']) ?>
-                                <?= Html::a('Modificar Artículo', ['modificar-articulo', 'Tienda_id' => $tienda->id], ['class' => 'btn btn-primary']) ?>
-                                <?= Html::a('Actualizar Tienda', ['update', 'id' => $tienda->id], ['class' => 'btn btn-warning']) ?>
-                            </p>
-                        </div>
+                            <?= Html::a('Crear Oferta', ['crear-oferta', 'Tienda_id' => $tienda->id], ['class' => 'btn btn-success']) ?>
+                            <?= Html::a('Modificar Oferta', ['modificar-oferta', 'Tienda_id' => $tienda->id], ['class' => 'btn btn-primary']) ?>
+                            <?= Html::a('Eliminar Oferta', ['eliminar-oferta', 'Tienda_id' => $tienda->id], ['class' => 'btn btn-danger']) ?>
+                            <?= Html::a('Crear Artículo', ['crear-articulo', 'Tienda_id' => $tienda->id], ['class' => 'btn btn-success']) ?>
+                            <?= Html::a('Eliminar Artículo', ['eliminar-articulo', 'Tienda_id' => $tienda->id], ['class' => 'btn btn-danger']) ?>
+                            <?= Html::a('Modificar Artículo', ['modificar-articulo', 'Tienda_id' => $tienda->id], ['class' => 'btn btn-primary']) ?>
+                        </p>
                     </div>
                 </div>
             <?php endforeach; ?>
         </div>
     <?php else: ?>
-        <p>No eres dueño de ninguna tienda.</p>
+        <p>No hay tiendas disponibles.</p>
     <?php endif; ?>
 </div>
